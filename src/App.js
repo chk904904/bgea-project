818ae9d4f401b23aa0306106f378d7cc4e0d9344
chk904904 <60486519+chk904904@users.noreply.github.com>
2020-10-13 03:52:10 -0400

    Update app js file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import Contribute from './components/Contribute';
-import Slogan from './components/Slogan';
-import Story from './components/Story';
-import logo from './logo.svg';
+import React from "react";
+import Contribute from "./components/Contribute";
+import Slogan from "./components/Slogan";
+import Story from "./components/Story";
+import logo from "./logo.svg";
+import FirstSession from "./components/firstSession";
+import MyNavBar from "./components/myNavbar";
 
 function App() {
   return (
     <>
-      <Slogan />
+      <MyNavBar />
+      <FirstSession />
       <Story />
     </>
   );
